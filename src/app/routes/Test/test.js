7c4374ffafe7e562ba0cd8081770ce28e5e1ea90
unknown <cesar@firstfactory.com>
2019-02-10 22:20:37 -0600

    'Initialcommit'

---
+++ src/app/routes/Test/test.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ContainerHeader from 'components/ContainerHeader/index';
+import Gallery from '../../../components/dashboard/Common/PhotoCollage/index';
+
+class Test extends React.Component {
+
+    render() {
+        return (
+            <div className="app-wrapper">
+                <ContainerHeader match={this.props.match} />
+                <div className="d-flex justify-content-center">
+                   <Gallery />
+                   <Gallery />
+                   <Gallery />
+                </div>
+
+            </div>
+        );
+    }
+}
+
+export default Test;
