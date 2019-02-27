b92e7a56d8140111ed4d71bcc7f2399f0cbc68e1
unknown <cesar@firstfactory.com>
2019-02-27 13:41:38 -0800

    added styles to the domains

--- src/app/routes/Domains/domains.js
+++ src/app/routes/Domains/domains.js
@@ -3,19 +3,24 @@ import SearchBox from '../../../components/SearchBox/index';
 import React from 'react';
 import ContainerHeader from 'components/ContainerHeader/index';
 import IntlMessages from 'util/IntlMessages';
+import Card from '../../../components/Cards/Sample/index';
 
 class SamplePage extends React.Component {
-
+    
     render() {
         return (
             <div className="app-wrapper">
                 <ContainerHeader match={this.props.match} title={<IntlMessages id="pages.Domains"/>}/>
-                <SearchBox styleName="d-none d-lg-block"
+                <Card>
+                <SearchBox styleName="d-lg-block"
                 placeholder="Search for a domain here"
                             //    onChange={this.updateSearchText.bind(this)}
                             //    value={this.state.searchText}
                                />
+                </Card>
+                <Card>
                 <OrderTable />
+                </Card>
             </div>
         );
     }
--- src/components/SearchBox/index.js
+++ src/components/SearchBox/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
+import { Button } from 'reactstrap'
 
 const SearchBox = ({styleName, placeholder, onChange, value}) => {
 
     return (
         <div className={`search-bar right-side-icon bg-transparent ${styleName}`}>
             <div className="form-group">
-                <input className="form-control border-0" type="search" placeholder={placeholder} onChange={onChange}
+                <input className="form-control border-solid-purple" type="search" placeholder={placeholder} onChange={onChange}
                        value={value}/>
                 <button className="search-icon"><i className="zmdi zmdi-search zmdi-hc-lg"/></button>
+                <Button className="button-link margin-top-3">Search</Button>
             </div>
+          
         </div>
     )
 };
