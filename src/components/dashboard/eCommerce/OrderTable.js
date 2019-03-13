420c0c4c87222efd21167851fe8f6684d1f36bd1
FF_Cesar <ff_cesar@FF-Admins-MacBook-Pro.local>
2019-03-13 17:35:27 -0600

    bootstrap changes

--- src/components/dashboard/eCommerce/OrderTable.js
+++ src/components/dashboard/eCommerce/OrderTable.js
@@ -1,29 +1,37 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import OrderTableCell from './OrderTableCell';
+import { Button } from 'reactstrap';
 
 
 
 class OrderTable extends Component {
     render() {
-        const {data} = this.props;
+        const { data } = this.props;
         return (
             <div className="table-responsive-material">
                 <table className="default-table table-unbordered table table-sm table-hover">
                     <thead className="th-border-b">
-                    <tr>
-                        <th>#</th>
-                        <th>Domain Name</th>
-                        <th>Provider</th>
-                        <th className="status-cell text-right">Price</th>
-                        <th></th>
-                    </tr>
+                        <tr>
+                            <th>#</th>
+                            <th>Domain Name</th>
+                            <th>Provider</th>
+                            <th className="status-cell text-right">Price</th>
+                            <th></th>
+                        </tr>
                     </thead>
                     <tbody>
-                    {data.map(data => {
-                        return (
-                            <OrderTableCell key={data.id} data={data}/>
-                        );
-                    })}
+                        {data.map(data => {
+                            return (
+                                <React.Fragment>
+                                    <tr className="d-md-none">
+                                        <Button className="button-link">
+                                            Buy
+                                    </Button>
+                                    </tr>
+                                    <OrderTableCell key={data.id} data={data} />
+                                </React.Fragment>
+                            );
+                        })}
                     </tbody>
                 </table>
             </div>
